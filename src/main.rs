da76e404c98ba3936f77ea78069f9ff0bdc5ebe8
DanielPitfield <danielpitfield1@gmail.com>
2023-07-27 18:38:58 +0100

    Day 1 - part 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-fn main() {
-    let values =  
+fn main() { 
+    let values: Vec<u16> =  
     // Include/Read the file as a string
     include_str!("./Day1.txt")
     // Get string slices (of each line)
@@ -7,12 +7,33 @@ fn main() {
     // Parse each line's value to number
     .map(|line| line.parse().unwrap())
     // Convert into more usable collection of a vector
-    .collect::<Vec<u16>>();
+    .collect();
+    
+    part1(values.clone());
+    part2(values.clone());
+}
 
-    // Get subslices (pairs) from the collection
+fn part1(values: Vec<u16>) {
+    // Get subslices (pairs)
     let pairs = values.windows(2);
 
-    // Count the occurences of a value being greater than value before it in the collection
+    // Count the occurences of the second value in the pair being greater than the first value
+    let mut counter: u16 = 0;
+    for pair in pairs {
+        if pair[1] > pair[0] { counter += 1; }
+    }
+
+    println!("{}", counter);
+}
+
+fn part2(values: Vec<u16>) {
+    // Get three value sliding windows
+    let windows = values.windows(3);
+    // Sum together the three values within each window
+    let sum_values: Vec<u16> = windows.map(|window| window.iter().sum()).collect();
+    
+    let pairs = sum_values.windows(2);
+
     let mut counter: u16 = 0;
     for pair in pairs {
         if pair[1] > pair[0] { counter += 1; }
