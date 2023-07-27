b6908016efe22e92ccbeba16e3303bbe149abc1f
DanielPitfield <danielpitfield1@gmail.com>
2023-07-27 18:14:46 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
+    let values =  
     // Include/Read the file as a string
-    let file_str = include_str!("./Day1.txt");
-    // String slices (of each line)
-    let lines = file_str.lines();
-    // Parse into a number
-    let values = lines.map(|line| line.parse().unwrap());
+    include_str!("./Day1.txt")
+    // Get string slices (of each line)
+    .lines()
+    // Parse each line's value to number
+    .map(|line| line.parse().unwrap())
     // Convert into more usable collection of a vector
-    let collection = values.collect::<Vec<u16>>();
+    .collect::<Vec<u16>>();
+
     // Get subslices (pairs) from the collection
-    let pairs = collection.windows(2);
+    let pairs = values.windows(2);
 
     // Count the occurences of a value being greater than value before it in the collection
     let mut counter: u16 = 0;
@@ -16,5 +18,5 @@ fn main() {
         if pair[1] > pair[0] { counter += 1; }
     }
 
-    println!("{}",counter);
+    println!("{}", counter);
 }
