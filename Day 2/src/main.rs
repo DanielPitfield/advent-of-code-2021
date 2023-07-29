87276028cbc04279b5e08c02935caa92e5c46daa
DanielPitfield <danielpitfield1@gmail.com>
2023-07-29 18:09:16 +0100

    Day 2 - part 2

--- Day 2/src/main.rs
+++ Day 2/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     .collect();
     
     part1(&lines);
+    part2(&lines);
 }
 
 fn part1(lines: &Vec<&str>) {
@@ -12,9 +13,12 @@ fn part1(lines: &Vec<&str>) {
     let mut horizontal_position = 0;
 
     for line in lines {
+        // Split to get the direction and the value
         let parts: Vec<&str> = line.split(" ").collect();
-
+        // Parse the value to number
         let value: usize =  parts[1].parse().unwrap();
+
+        // Forward increases horizontal position
         if parts[0] == "forward" { horizontal_position += value; }
         // Down increases the depth
         else if parts[0] == "down" { depth += value; }
@@ -24,4 +28,28 @@ fn part1(lines: &Vec<&str>) {
 
     let result: usize = depth * horizontal_position;
     println!("Part 1: {result}");
+}
+
+fn part2(lines: &Vec<&str>) {
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut horizontal_position = 0;
+
+    for line in lines {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let value: usize =  parts[1].parse().unwrap();
+
+        if parts[0] == "forward" { 
+            horizontal_position += value;
+            // Also now increases depth by aim multiplied by the value
+            depth += (aim * value);
+        }
+        // Increases aim
+        else if parts[0] == "down" { aim += value; }
+        // Decreases aim
+        else { aim -= value; }
+    }
+
+    let result: usize = depth * horizontal_position;
+    println!("Part 2: {result}");
 }
