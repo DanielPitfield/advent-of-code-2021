9d0932495891556bb370997c06a68642560d0e95
DanielPitfield <danielpitfield1@gmail.com>
2023-08-06 16:19:00 +0100

    Fix num_columns

--- Day 3/src/main.rs
+++ Day 3/src/main.rs
@@ -9,7 +9,8 @@ fn part1(rows: &Vec<&str>) {
     let mut epsilon_binary_string = String::new();
 
     // How many bits are there in the binary strings (all rows have the same number of bits so just look at the first row)?
-    let num_columns = rows[0].split("").count();
+    let num_columns = rows[0].len();
+
     // Map each row to a vector of the bits/characters
     let grid: Vec<Vec<&str>> = rows.iter().map(|value| value.split("").collect()).collect();
 
@@ -18,7 +19,7 @@ fn part1(rows: &Vec<&str>) {
         let column_values: Vec<&str> = grid.iter().map(|row| row[column_number]).collect();
 
         // More 1 bits than 0 bits?
-        if column_values.iter().filter(|&x| x == "1").count() >= column_values.iter().count() / 2 {
+        if column_values.iter().filter(|&x| x.to_string() == "1").count() >= column_values.iter().count() / 2 {
             gamma_binary_string.push_str("1");
             epsilon_binary_string.push_str("0");
         } else {
