0ddba0685e0a72c6f752b388f4fc2509f2614b5a
DanielPitfield <danielpitfield1@gmail.com>
2023-08-06 16:39:24 +0100

    Fix 'off-by-one' loop condition

--- Day 3/src/main.rs
+++ Day 3/src/main.rs
@@ -5,21 +5,34 @@ fn main() {
 }
 
 fn part1(rows: &Vec<&str>) {
+    // Will hold the most common bit in each column
     let mut gamma_binary_string = String::new();
+    // Will hold the least common bit in each column
     let mut epsilon_binary_string = String::new();
 
     // How many bits are there in the binary strings (all rows have the same number of bits so just look at the first row)?
     let num_columns = rows[0].len();
-
     // Map each row to a vector of the bits/characters
-    let grid: Vec<Vec<&str>> = rows.iter().map(|value| value.split("").collect()).collect();
+    let grid: Vec<Vec<char>> = rows.iter().map(|value| value.chars().collect()).collect();
 
-    for column_number in 0..(num_columns - 1) {
+    for column_number in 0..(num_columns) {
         // The values/bits for just the currently iterated column
-        let column_values: Vec<&str> = grid.iter().map(|row| row[column_number]).collect();
+        let column_values: Vec<char> = grid.iter().map(|row| row[column_number]).collect();
+
+        // How many bits are "0"?
+        let num_zero_bits = column_values
+            .iter()
+            .filter(|&x| x.to_string() == "0")
+            .count();
+
+        // How many bits are "1"?
+        let num_one_bits = column_values
+            .iter()
+            .filter(|&x| x.to_string() == "1")
+            .count();
 
         // More 1 bits than 0 bits?
-        if column_values.iter().filter(|&x| x.to_string() == "1").count() >= column_values.iter().count() / 2 {
+        if num_one_bits > num_zero_bits {
             gamma_binary_string.push_str("1");
             epsilon_binary_string.push_str("0");
         } else {
